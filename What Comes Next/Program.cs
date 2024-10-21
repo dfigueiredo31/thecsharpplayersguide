5fd4d13aa2b3e3ff3ffd18bf814928a7379a7f0f
dfigueiredo31 <david.gabriel.31@gmail.com>
2024-10-21 22:58:24 +0100

    What comes next Challenge

--- What Comes Next/Program.cs
+++ What Comes Next/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello....Is it me you're looking for?");
+            Console.WriteLine("I can see it in your eyes, I can see it your smile...");
         }
     }
 }
