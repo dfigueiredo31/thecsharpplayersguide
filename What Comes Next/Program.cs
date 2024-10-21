2890121d5b11554cd05b0d3cfce1f4307fbf0bc0
dfigu <dfigu@VILA_REAL>
2024-10-21 21:56:34 +0100

    Add .gitattributes, .gitignore, and README.md.

--- What Comes Next/Program.cs
+++ What Comes Next/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace HelloWorld
+﻿namespace WhatComesNext
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello....Is it me you're looking for?");
         }
     }
 }
