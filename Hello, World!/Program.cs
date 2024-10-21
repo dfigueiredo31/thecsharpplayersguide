5c91197f2e42d1b54a97a9a16a19558dd5454903
dfigu <dfigu@VILA_REAL>
2024-10-21 21:56:37 +0100

    Add project files.

---
+++ Hello, World!/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hello__World_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
