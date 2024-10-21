7920bf7f983ba67825449f3ad7be7364b4a69821
dfigueiredo31 <david.gabriel.31@gmail.com>
2024-10-21 23:14:23 +0100

    Reorganization of the projects by level The Makings of a Programmer Challenge

---
+++ The Makings of a Programmer/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Objectives:
+ * 
+ * Make a program with 5 Console.WriteLine statements in it.
+ * 
+ * **/
+
+namespace TheMakingsofaProgrammer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("statment {0}", 1);
+            Console.WriteLine("statment {0}", 2);
+            Console.WriteLine("statment {0}", 3);
+            Console.WriteLine("statment {0}", 4);
+            Console.WriteLine("statment {0}", 5);
+        }
+    }
+}
