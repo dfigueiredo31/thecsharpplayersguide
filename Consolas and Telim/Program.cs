881ca8742d5d2ab31ccceb017469823836ec7c83
dfigueiredo31 <david.gabriel.31@gmail.com>
2024-10-22 22:01:07 +0100

    Consolas and Telim Challenge

---
+++ Consolas and Telim/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Objectives:
+ * 
+ * Make a program that runs as shown above [on the book], including a name from the user
+ * 
+ * **/
+
+
+namespace Consolas_and_Telim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("Bread is ready\nWho is the bread for?");
+            name = Console.ReadLine();
+            Console.WriteLine($"Noted, {name} got bread.");
+        }
+    }
+}
