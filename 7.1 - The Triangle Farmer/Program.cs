83a20daa2391592d5b3db969e6b71169a9b783ae
dfigueiredo31 <david.gabriel.31@gmail.com>
2024-12-21 16:32:29 +0000

    The Triangle Farmer challenge - Level 7

---
+++ 7.1 - The Triangle Farmer/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _7._1___The_Triangle_Farmer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tArea, tBase, tHeight;
+
+            Console.WriteLine(":::::: Triangle Area Calculator ::::::");
+            
+            Console.WriteLine("Enter triangle base: ");
+            while(!double.TryParse(Console.ReadLine(), out tBase))
+                Console.WriteLine("Invalid base value");
+
+            Console.WriteLine("Enter triangle height: ");
+            while(!double.TryParse(Console.ReadLine(), out tHeight))
+                Console.WriteLine("Invalid height value");
+
+            tArea = (tBase *  tHeight) / 2;
+            Console.WriteLine($"The triangle area is {tArea}");
+        }
+    }
+}
