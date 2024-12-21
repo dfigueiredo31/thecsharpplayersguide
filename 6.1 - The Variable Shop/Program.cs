52fb1d07a5a03be2b98c7892549254e2c4c2fccf
dfigueiredo31 <david.gabriel.31@gmail.com>
2024-12-21 15:07:34 +0000

    Challenges added:
    6.1 - The Variable Shop
    6.2 - The Variable Shop Returns

---
+++ 6.1 - The Variable Shop/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _6._1___The_Variable_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //:::::::: integer types ::::::::
+            //signed
+            int a = -1;
+            long b = -2;
+            short c = -3;
+            sbyte d = -4;
+
+            //unsigned
+            uint e = 5;
+            ulong f = 6;
+            ushort g = 7;
+            byte h = 8;
+
+            //:::::::: floating point types ::::::::
+            float i = 9.5f;
+            double j = 50.100;
+            decimal k = 100m;
+
+            char l = 'A';
+            bool m = false;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(l);
+            Console.WriteLine(m);
+
+        }
+    }
+}
---
+++ 6.2 - The Variable Shop Returns/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _6._2___The_Variable_Shop_Returns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //:::::::: integer types ::::::::
+            //signed
+            int a = -1;
+            long b = -2;
+            short c = -3;
+            sbyte d = -4;
+
+            //unsigned
+            uint e = 5;
+            ulong f = 6;
+            ushort g = 7;
+            byte h = 8;
+
+            //:::::::: floating point types ::::::::
+            float i = 9.5f;
+            double j = 50.100;
+            decimal k = 100m;
+
+            char l = 'A';
+            bool m = false;
+
+            a = 2;
+            b = 3;
+            c = 4;
+            d = 5;
+            e = 6;
+            f = 7;
+            g = 8;
+            h = 9;
+            i = 10;
+            j = 11;
+            k = 12;
+            l = 'X';
+            m = true;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(l);
+            Console.WriteLine(m);
+
+        }
+    }
+}
